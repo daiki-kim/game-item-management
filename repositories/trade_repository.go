13643515193897749ca4f0a20b6b564a4cb43fa1
Daiki Kim <kimdaiki5021@gmail.com>
2024-05-30 07:21:22 +0900

    Create 'CreateNewTrade()' at trade_repo

--- repositories/trade_repository.go
+++ repositories/trade_repository.go
@@ -1,10 +1,13 @@
 package repositories
 
 import (
+	"game-item-management/models"
+
 	"gorm.io/gorm"
 )
 
 type ITradeRepository interface {
+	CreateNewTrade(newTrade models.Trade) (*models.Trade, error)
 }
 
 type TradeRepository struct {
@@ -13,4 +16,12 @@ type TradeRepository struct {
 
 func NewTradeRepository(db *gorm.DB) ITradeRepository {
 	return &TradeRepository{db: db}
+}
+
+func (r *TradeRepository) CreateNewTrade(newTrade models.Trade) (*models.Trade, error) {
+	result := r.db.Create(&newTrade)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &newTrade, nil
 }

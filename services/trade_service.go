f2ec8c1f6d957afc8db5dbd0ab8840d82d8be616
Daiki Kim <kimdaiki5021@gmail.com>
2024-05-31 05:33:10 +0900

    Create 'CreateNewTrade()' at tarde_service

--- services/trade_service.go
+++ services/trade_service.go
@@ -1,12 +1,34 @@
 package services
 
 import (
+	"game-item-management/models"
 	"game-item-management/repositories"
 )
 
 type ITradeService interface {
+	CreateNewTrade(itemId, toUserId uint) (*models.Trade, error)
 }
 
 type TradeService struct {
-	repository repositories.ITradeRepository
+	itemRepository  repositories.IItemRepository
+	tradeRepository repositories.ITradeRepository
+}
+
+func NewTradeService(itemRepository repositories.IItemRepository, tradeRepository repositories.ITradeRepository) ITradeService {
+	return &TradeService{itemRepository: itemRepository, tradeRepository: tradeRepository}
+}
+
+func (s *TradeService) CreateNewTrade(itemId, toUserId uint) (*models.Trade, error) {
+	item, err := s.itemRepository.FindItemById(itemId)
+	if err != nil {
+		return nil, err
+	}
+
+	trade := models.Trade{
+		Is_Accepted: false,
+		ItemID:      itemId,
+		FromUserID:  item.UserID,
+		ToUserID:    toUserId,
+	}
+	return s.tradeRepository.CreateNewTrade(trade)
 }

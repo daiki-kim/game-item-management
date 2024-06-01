d076cc76c61afee7fff159063a1fb86228b7366b
Daiki Kim <kimdaiki5021@gmail.com>
2024-06-01 18:14:53 +0900

    Modify model's primary key

--- models/item.go
+++ models/item.go
@@ -6,6 +6,6 @@ type Item struct {
 	gorm.Model
 	Name        string `gorm:"not null"`
 	Description string
-	UserID      uint `gorm:"not null"`
-	User        User `gorm:"foreignKey:UserID"`
+	UserID      uint `gorm:"not null;foreignKey:UserID"`
+	// User        User `gorm:"foreignKey:UserID"`
 }
